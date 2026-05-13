d97878aeb383f1e151b53a8cbd8cec287d8c346c
Bruno Moreira <git@pocketarc.com>
2026-05-13 05:45:32 -0400

    Migrate to Cloudflare Workers static assets with Plausible proxy via tvsort.com.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,19 @@ import {MDXProvider} from '@mdx-js/react';
 import mdxComponents from "../lib/mdxComponents";
 import Script from "next/script";
 
+const plausibleUrl = "https://tvsort.com";
+
 export default function MyApp({Component, pageProps}: AppProps) {
     return <>
         <MDXProvider components={mdxComponents}>
             <Component {...pageProps} />
         </MDXProvider>
-        <Script src="https://plausible.io/js/script.js" data-domain="diffusiondepot.app" strategy="afterInteractive"/>
+        <Script id="plausible-init" strategy="beforeInteractive">
+            {`window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init({endpoint:"${plausibleUrl}/api/event"})`}
+        </Script>
+        <Script
+            src={`${plausibleUrl}/js/pa-q5g6LH4OrSECkV-0DbrH8.js`}
+            strategy="afterInteractive"
+        />
     </>
 }
